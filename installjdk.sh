669e6c9c6a5f3c7e1f110a2835aeffdcb24499ab
gezhiwei <gezhiwei8899@gmail.com>
2018-10-01 11:31:03 +0800

    fisrtcommit

--- installjdk.sh
+++ installjdk.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-1
+yum install -y wget
+wget https://github.com/utilcollect/java-install-sh/releases/download/1.0/jdk-8u181-linux-x64.rpm
+rpm -ivh jdk-8u181-linux-x64.rpm
+echo "JAVA_HOME=/usr/java/default" >> /etc/profile
+echo "export JAVA_HOME" >> /etc/profile
+echo "PATH=\$JAVA_HOME/bin:$PATH" >> /etc/profile
+source /etc/profile
