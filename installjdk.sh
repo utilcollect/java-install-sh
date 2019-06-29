42c657cd7cd362f25e2336286f5779c2882bf341
gezhiwei <gezhiwei8899@gmail.com>
2019-06-29 09:20:02 +0800

    change the binary to my own gitlab

--- installjdk.sh
+++ installjdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y wget
-wget https://github.com/utilcollect/java-install-sh/releases/download/1.0/jdk-8u181-linux-x64.rpm
-rpm -ivh jdk-8u181-linux-x64.rpm
+wget http://suzhouxiaoge.synology.me:30000/binary/jdk/raw/master/jdk-8u212-linux-x64.rpm
+rpm -ivh jdk-8u212-linux-x64.rpm
 echo "JAVA_HOME=/usr/java/default" >> /etc/profile
 echo "export JAVA_HOME" >> /etc/profile
 echo "PATH=\$JAVA_HOME/bin:$PATH" >> /etc/profile
