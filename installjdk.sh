c571bdc89334bc40e699a0ed3056400ba00d9af6
gezhiwei <gezhiwei8899@gmail.com>
2019-12-26 15:55:19 +0800

    Update installjdk.sh

--- installjdk.sh
+++ installjdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y wget
-wget http://suzhouxiaoge.synology.me:30000/binary/jdk/raw/master/jdk-8u212-linux-x64.rpm
+wget https://code.aliyun.com/gezhiwei/jdk8/raw/d6308cd69935d14ca98e0f7f46e35acfede76f83/jdk-8u212-linux-x64.rpm
 rpm -ivh jdk-8u212-linux-x64.rpm
 echo "JAVA_HOME=/usr/java/default" >> /etc/profile
 echo "export JAVA_HOME" >> /etc/profile
